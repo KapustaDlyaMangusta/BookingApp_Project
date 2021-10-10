325298b55feb57f629973389609740767ca31ce5
KapustaDlyaMangusta <maksick2005@gmail.com>
2021-10-11 00:23:27 +0300

    Start__BETA

--- BookingApp_Project/Controllers/UserDoingsController.cs
+++ BookingApp_Project/Controllers/UserDoingsController.cs
@@ -8,26 +8,26 @@ using System.Threading.Tasks;
 namespace BookingApp_Project.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("app/user")]
+    public class AppUserController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<AppUserController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public AppUserController(ILogger<AppUserController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<AppUserController> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new AppUserController
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
